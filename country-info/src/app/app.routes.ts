30be7edf4b7a67b94eba9b3290a31459c610dd20
Oleksandra  Vereshchagina <alexxa.vereshchagina@gmail.com>
2024-09-10 16:33:24 +0300

    app init component

--- country-info/src/app/app.routes.ts
+++ country-info/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CountryComponent } from './pages/country/country.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'country/:countryCode', component: CountryComponent },
+];
